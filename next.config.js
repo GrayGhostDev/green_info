ac2529779881ec11cdd7ce3618117c75a2ec212c
Curtis Jemison <rlb01@svbots.com>
2025-01-17 21:43:01 -0500

    Configure GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,25 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  basePath: '/green_info',
   images: {
-    domains: ['localhost'],
-    unoptimized: true
-  },
-  // Ensure images are copied to the public directory
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(png|jpe?g|gif|svg)$/i,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            publicPath: '/_next/static/images/',
-            outputPath: 'static/images/',
-          },
-        },
-      ],
-    });
-    return config;
+    unoptimized: true,
   },
 }
 
